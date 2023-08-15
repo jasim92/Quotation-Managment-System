107cc09d1ef7f9b0368a699d16965e9f0e02fa2d
jasim <jassu92@yahoo.com>
2023-08-15 13:49:00 +0400

    login functionality improved by using jwt, welcome message for logged users, and bug fixed

--- login.js
+++ login.js
@@ -7,7 +7,7 @@ function login() {
 
 
     if (username.length > 0 && password.length > 0) {
-        getUser(username, password);
+        getJWToken(username, password);
     }
     else {
         alert("please provide username/pasword");
@@ -22,7 +22,7 @@ function getUser(user, password) {
         .then(response => response.json())
         .then(result => {
             let isAuthenticated = false;
-           for (const userObj of result ) {
+            for (const userObj of result) {
                 const { username, secretkey } = userObj;
                 console.log(username);
                 if (user === username && password === secretkey) {
@@ -31,12 +31,55 @@ function getUser(user, password) {
                     window.location.href = 'index.html';
                     break;
                 }
-                
+
             }
 
-            if(!isAuthenticated) {
+            if (!isAuthenticated) {
                 alert("Wrong username/Password");
             }
 
         });
-    }
+}
+
+async function getJWToken(user, pass) {
+    let options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            identifier: user,
+            password: pass
+        }),
+    };
+
+    await fetch("http://192.168.0.132:1337/api/auth/local", options)
+        .then(response => {
+            if(response.status === 200){
+                return  response.json();
+            }
+            else{
+                throw new Error("Authentication failed");
+            }
+           
+        } )
+        .then(result => {
+
+            const jwtoken = result.jwt;
+            
+
+            localStorage.setItem("jwtToken", jwtoken);
+            localStorage.setItem("user", user);
+            const getJWT = localStorage.getItem("jwtToken");
+            if (getJWT) {
+                console.log(getJWT);
+                alert("Welcome " + user);
+                window.location.href = 'index.html';
+            }
+        }).catch(error =>{
+            console.error("Authentication failed:", error);
+        alert("Wrong username/Password");
+        window.location.href = "loginPage.html";
+        });
+}
+
--- script.js
+++ script.js
@@ -1,19 +1,40 @@
+const jwtToken = localStorage.getItem("jwtToken");
+    if (!jwtToken) {
+        // If not authenticated, redirect to the login page
+        window.location.href = "loginPage.html";
+    
+    }
 const mainListContainer = document.getElementById("main");
 const mainTableBody = document.getElementById("tablesBody");
 const formElement = document.getElementById('quotationForm');
 const quotationElement = document.getElementById('quotationNumber');
+const createrElement = document.getElementById('creater');
 const searchForm = document.getElementById('form');
 const total_value = document.getElementById('total-value');
 const addButton = document.getElementById('addButton');
+const logoutButton = document.getElementById('logout');
+const usernameText = document.getElementById('userNameWelcome');
+
+usernameText.textContent = "Welcome, "+localStorage.getItem("user").toUpperCase();
+
 const SERVER_URL = "http://192.168.0.132:1337/api/quotations";
 var newQueryUrl = '';
 let quotationNumberForForm;
 getDropdownValue();
 
 // to increament quotation number based on previous number
-addButton.addEventListener("click", function (){
-    quotationElement.value = "E1-Q-2023-"+quotationNumberForForm;
-})
+addButton.addEventListener("click", function () {
+    const year = new Date().getFullYear();
+    quotationElement.value = "E1-Q-" + year + "-" + quotationNumberForForm;
+
+    createrElement.value = localStorage.getItem("user");
+
+    const currentDate = new Date().toISOString().split('T')[0];
+    document.getElementById('date').value = currentDate;
+
+    
+
+});
 
 document.getElementById("saveButton").addEventListener("click", async function (e) {
     e.preventDefault();
@@ -31,11 +52,21 @@ document.getElementById("saveButton").addEventListener("click", async function (
             }
             else {
 
+                if(currentElement.name==="currency" && currentElement.value==="usd")
+                {
+                    const newValue = data.value * 3.67;
+                    data['value'] = newValue;
+                    
+                }
                 data[currentElement.name] = currentElement.value;
+
+               
+
             }
 
         }
     }
+    console.log(data);
 
     if (erroMessages.length > 0) {
         const errorMsg = erroMessages.join(', ');
@@ -51,6 +82,7 @@ document.getElementById("saveButton").addEventListener("click", async function (
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
+                    Authorization: "Bearer "+localStorage.getItem("jwtToken"),
                 },
                 body: JSON.stringify({
                     data: data,
@@ -81,6 +113,9 @@ document.getElementById("saveButton").addEventListener("click", async function (
                 const uploadResponse = await fetch('http://192.168.0.132:1337/api/upload/', {
                     method: 'POST',
                     body: formData,
+                    headers: {
+                        Authorization: "Bearer "+localStorage.getItem("jwtToken"),
+                    }
                 });
 
                 if (uploadResponse.ok) {
@@ -107,19 +142,21 @@ document.getElementById("saveButton").addEventListener("click", async function (
     }
 });
 
-const getQuotationList = (queryUrl, searching, selectedValue) => {
+const getQuotationList = async (queryUrl, searching, selectedValue) => {
     mainListContainer.innerHTML = "";
     let options = {
         method: "GET",
+        headers: {
+            Authorization: "Bearer "+localStorage.getItem("jwtToken"),
+        }
     };
     if (searching == false) {
         newQueryUrl = queryUrl + '?populate=*';
 
     } else {
         newQueryUrl = queryUrl + '&populate=*';
-
     }
-    fetch(newQueryUrl, options)
+    await fetch(newQueryUrl, options)
         .then(response => response.json())
         .then(result => {
 
@@ -128,8 +165,8 @@ const getQuotationList = (queryUrl, searching, selectedValue) => {
                 const filteredRows = []; // Array to store the filtered rows
                 const quoteArray = [];
                 result.data.forEach((element, index) => {
-                    
-                    const { clientName, status, quotationNumber, issueDate, file, creater, value } = element.attributes;
+
+                    const { clientName, status, quotationNumber, issueDate, file, creater, value, productName } = element.attributes;
                     quoteArray.push(parseInt(quotationNumber.slice(10,)));
 
                     if (!selectedValue || creater.includes(selectedValue)) {
@@ -141,6 +178,7 @@ const getQuotationList = (queryUrl, searching, selectedValue) => {
                         <th scope="row">${index + 1}</th>
                         <td>${clientName}</td>
                         <td>${quotationNumber}</td>
+                        <td>${productName}</td>
                         <td>${status}</td>
                         <td>${issueDate}</td>
                         <td>${creater}</td>
@@ -159,13 +197,20 @@ const getQuotationList = (queryUrl, searching, selectedValue) => {
                     tableBody.appendChild(row);
 
                 });
-                
-                quotationNumberForForm = Math.max(...quoteArray)+1;
+
+                quotationNumberForForm = Math.max(...quoteArray) + 1;
+            } else {
+
+                getServerStatus("There is no data in Server");
+
             }
 
 
         })
-        .catch(error => console.log('error', error));
+        .catch(error => {
+            console.log('error', error);
+            getServerStatus("Server is not Running. Please contact administrator.");
+        });
 }
 
 
@@ -201,20 +246,20 @@ function getDropdownValue() {
             const selectedValue = item.getAttribute("data-value");
 
 
-            if (selectedValue === "salah") {
+            if (selectedValue == "Salah") {
                 mainListContainer.innerHTML = '';
                 console.log("Selected value:", selectedValue);
-                getQuotationList(SERVER_URL, false, "salah");
+                getQuotationList(SERVER_URL, false, "Salah");
             }
-            else if (selectedValue === "zeeshan") {
+            else if (selectedValue == "Zeeshan") {
                 mainListContainer.innerHTML = '';
                 console.log("Selected value:", selectedValue);
-                getQuotationList(SERVER_URL, false, "zeeshan");
+                getQuotationList(SERVER_URL, false, "Zeeshan");
             }
-            else if (selectedValue === "zaheer") {
+            else if (selectedValue == "Mohammed") {
                 mainListContainer.innerHTML = '';
-                console.log("Selected value:", "zaheer");
-                getQuotationList(SERVER_URL, false, selectedValue);
+                console.log("Selected value:", selectedValue);
+                getQuotationList(SERVER_URL, false, "Mohammed");
             }
             else {
                 mainListContainer.innerHTML = '';
@@ -233,3 +278,35 @@ function formAlertFunction(alertMsg) {
     formAlert.textContent = alertMsg;
     formAlert.style.display = 'block'; // Show the alert
 }
+
+function getServerStatus(message) {
+    const errorMsg = document.getElementById('error-msg');
+    if (errorMsg) {
+        const divElement = document.createElement("div");
+        divElement.classList.add("alert", "alert-danger");
+        divElement.setAttribute("role", "alert");
+        divElement.innerHTML = `${message}`;
+
+        errorMsg.appendChild(divElement);
+    }
+    else {
+        console.warn('Element with id "error-msg" not found.');
+    }
+}
+
+logoutButton.addEventListener("click", ()=>{
+    logOutUser();
+});
+
+function logOutUser() {
+    // Remove the JWT token from localStorage
+    localStorage.removeItem("jwtToken");
+    localStorage.removeItem("user");
+
+    // Redirect to the login page or any other appropriate action
+    window.location.href = "loginPage.html";
+}
+
+
+
+    
